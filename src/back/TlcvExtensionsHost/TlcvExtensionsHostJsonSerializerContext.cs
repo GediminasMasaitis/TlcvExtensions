328e3f904e976cf423a55a540463c2e502e42913
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-05-20 00:31:43 +0200

    Fix serialization issues

---
+++ src/back/TlcvExtensionsHost/TlcvExtensionsHostJsonSerializerContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using TlcvExtensionsHost.Models;
+
+namespace TlcvExtensionsHost;
+
+[JsonSerializable(typeof(QueryResponse))]
+[JsonSerializable(typeof(EngineInfo))]
+[JsonSerializable(typeof(FenRequest))]
+[JsonSerializable(typeof(FenResponse))]
+internal partial class TlcvExtensionsHostJsonSerializerContext : JsonSerializerContext;
